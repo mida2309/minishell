cbbc547e37945ac45bd9fea7a5353a1f3a49ec3d
mida2309 <79518516+mida2309@users.noreply.github.com>
2021-10-14 14:37:11 +0200

    Delete ori directory

--- ori/ast1/includes/env_utils.h
+++
@@ -1,9 +0,0 @@
-#ifndef ENV_UTILS_H
-# define ENV_UTILS_H
-
-# include "minishell.h"
-
-void	print_sorted(t_list *env);
-int		find_symbol(char *str, char sym);
-char	*get_env_val(char *env_name, t_list *envlist);
-#endif
--- ori/ast1/includes/sig_handlers.h
+++
@@ -1,9 +0,0 @@
-#ifndef SIG_HANDLERS_H
-# define SIG_HANDLERS_H
-# include "minishell.h"
-
-void	proc_signal_handler(int signum);
-void	signal_handler(int signo);
-void	handle_sigquit(void);
-void	handle_sigint(int sig);
-#endif
